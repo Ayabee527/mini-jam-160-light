0341967d0af40dc2b69d2fa209a1c22cb2e6da7f
pixemisti <66703857+Ayabee527@users.noreply.github.com>
2024-06-08 13:16:48 +0000

    layout change

--- light-jam/player/states/hook.gd
+++ light-jam/player/states/hook.gd
@@ -6,11 +6,13 @@ extends PlayerState
 @export var launch_window: Timer
 @export var launch_particles: GPUParticles2D
 
+var previous_vel: Vector2
 var grapple_point: Area2D
 
 func enter(msg:={}) -> void:
 	if msg.has("grapple"):
 		grapple_point = msg["grapple"]
+		previous_vel = player.linear_velocity
 		player.linear_velocity = Vector2.ZERO
 		player.gravity_scale = 0
 		player.latch_sound.play()
@@ -39,7 +41,11 @@ func exit() -> void:
 	player.gravity_scale = 0.5
 	player.hook_sound.stop()
 	if not launch_window.is_stopped():
-		player.apply_central_impulse(Vector2.UP * player.linear_velocity.length())
+		var dir_to_grapple_point = player.global_position.direction_to(grapple_point.global_position)
+		player.apply_central_impulse(
+			player.linear_velocity
+			+ ( player.linear_velocity.normalized() * previous_vel.length() * 0.25 )
+		)
 		launch_particles.restart()
 		player.launch_sound.play()
 
