ceadafc81500a6dec4144c2064279e5b632ea695
pixemisti <66703857+Ayabee527@users.noreply.github.com>
2024-06-08 09:50:01 +0000

    grapple start

---
+++ light-jam/player/hook_detect.gd
@@ -0,0 +1,49 @@
+class_name PlayerHookDetect
+extends Area2D
+
+@export var player: Player
+@export var grapple_highlight: Polygon2D
+
+var grapple_points: Array[Area2D]
+
+var sine: float = 0.0
+
+var targeted_grapple: Area2D
+
+func _process(delta: float) -> void:
+	global_position = get_global_mouse_position()
+	grapple_highlight.global_rotation_degrees += 180.0 * delta
+	
+	grapple_highlight.scale = Vector2.ONE * ((0.25 * sin(sine)) + 1.0)
+	
+	sine += 5.0 * delta
+	sine = fposmod(sine, TAU * 5.0)
+
+func update_highlight() -> void:
+	var closest: Area2D = null
+	var closest_dist: float = INF
+	for grapple: Area2D in grapple_points:
+		var dist = player.global_position.distance_to(grapple.global_position)
+		if dist < closest_dist:
+			closest_dist = dist
+			closest = grapple
+	
+	grapple_highlight.global_position = closest.global_position
+	targeted_grapple = closest
+
+func _on_area_entered(area: Area2D) -> void:
+	if not grapple_points.has(area):
+		grapple_points.append(area)
+		update_highlight()
+		grapple_highlight.show()
+
+
+func _on_area_exited(area: Area2D) -> void:
+	if grapple_points.has(area):
+		grapple_points.erase(area)
+	
+	if grapple_points.size() > 0:
+		update_highlight()
+	else:
+		targeted_grapple = null
+		grapple_highlight.hide()
--- light-jam/player/states/fall.gd
+++ light-jam/player/states/fall.gd
@@ -7,5 +7,5 @@ func physics_update(delta: float) -> void:
 		Vector2.RIGHT * player.get_move_axis() * move_speed
 	)
 	
-	if player.global_position.y > 256 + 16:
-		player.apply_central_impulse(Vector2.UP * 500.0)
+	#if player.global_position.y > 256 + 16:
+		#player.apply_central_impulse(Vector2.UP * 500.0)
